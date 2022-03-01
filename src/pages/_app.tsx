5ad948741592431a3a2ae788888b88de0066b5c9
Breno Norberto <brenonorberto@gmail.com>
2022-03-01 14:17:55 -0300

    Integrando faunaDB

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,15 @@
 import { AppProps } from 'next/app'; // Serve para tipar o App
 import { Header } from '../components/Header';
+import { SessionProvider } from 'next-auth/react';
+
 import '../styles/global.scss';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <SessionProvider session={pageProps.session}>
       <Header />
       <Component {...pageProps} />
-    </>
+    </SessionProvider>
   );
 }
 
---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,33 @@
+import { query as q } from 'faunadb';
+import NextAuth from 'next-auth';
+import GithubProvider from 'next-auth/providers/github';
+
+import { fauna } from '../../../services/fauna';
+
+export default NextAuth({
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+      authorization: {
+        params: {
+          scope: 'read:user'
+        }
+      }
+    })
+  ],
+  callbacks: {
+    async signIn({ user, account, profile }) {
+      const { email } = user;
+
+      console.log(user);
+      try {
+        await fauna.query(q.Create(q.Collection('users'), { data: { email } }));
+        return true;
+      } catch (error) {
+        console.log(error);
+        return false;
+      }
+    }
+  }
+});
