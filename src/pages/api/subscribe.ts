8694be3242f76fa9564071043c086c01d384288c
Breno Norberto <brenonorberto@gmail.com>
2022-03-02 16:15:29 -0300

    Criando chckout

---
+++ src/pages/api/subscribe.ts
@@ -0,0 +1,35 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import { stripe } from '../../services/stripe';
+import { getSession } from 'next-auth/react';
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method === 'POST') {
+    const session = await getSession({ req });
+
+    const stripeCustomer = await stripe.customers.create({
+      email: session.user.email
+      //metadata
+    });
+
+    const stripeCheckoutSession = await stripe.checkout.sessions.create({
+      customer: stripeCustomer.id,
+      payment_method_types: ['card'],
+      billing_address_collection: 'required',
+      line_items: [
+        {
+          price: 'price_1JwRxJJv4O0Vxz5DTX4OyQ6I',
+          quantity: 1
+        }
+      ],
+      mode: 'subscription',
+      allow_promotion_codes: true,
+      success_url: process.env.STRIPE_SUCESS_URL,
+      cancel_url: process.env.STRIPE_CANCEL_UR
+    });
+    return res.status(200).json({ sessionId: stripeCheckoutSession.id });
+  } else {
+    res.setHeader('Allow', 'POST');
+    res.status(405).end('Method not allowed');
+  }
+};
